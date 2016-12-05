fa3ad5a33364d7de833d11d9e72746130c43fb07
vkumar <vkumar@LT-63715.forbes.com>
2016-12-05 17:12:07 -0500

    Adding String Reverse Program

--- src/HackerRank/StringReverse.java
+++ src/HackerRank/StringReverse.java
@@ -6,36 +6,27 @@ import java.util.*;
 public class StringReverse {
 
 	public static void main (String[] args) {
-	
-	    //Input
-        Scanner sc= new Scanner(System.in);
-        int n=sc.nextInt();
-        String []s=new String[n+2];
-        for(int i=0;i<n;i++){
-            s[i]=sc.next();
-        }
-      	sc.close();
-      	
-      	// Did not undestand this part. - START
-    	Arrays.sort(s, 0, n, new Comparator<String>() {
 
-	        public int compare(String s1, String s2) {
-
-	            BigDecimal bd1 = new BigDecimal(s1);
-	            BigDecimal bd2 = new BigDecimal(s2);
-
-	            return bd1.compareTo(bd2) == 1 ? -1:bd1.compareTo(bd2) == -1 ? 1:0;                
-	        }
-	    });
-    	// Did not undestand this part. - END
-      	
-    	
-      	 //Output
-        for(int i=0;i<n;i++)
-        {
-            System.out.println(s[i]);
-        }
-    }
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		String output = reverse(input);
 		
 	}
+
+	private static String reverse(String input) {
+		String reverse = " ";
+		
+		if(input.length() <=1) {
+			reverse = input;
+		} else {
+			String[] anArray = input.split("\\s+"); // Regex split
+			
+			for(String item : anArray) {
+				reverse = item +" "+reverse;
+			}
+		}
+		
+		return reverse.trim(); // removes leading or trailing spaces 
+	}
+	
 }
